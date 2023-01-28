0c0f3dca6cb89e3e47b520b0208a16fade197e9b
roman1231341414 <120581429+roman1231341414@users.noreply.github.com>
2023-01-28 07:03:47 +0300

    дз 5 задачи 34 , 36

---
+++ zad_34/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2
+*/
+
+int getUserData(string message)
+{
+Console.WriteLine(message);
+int userData = int.Parse(Console.ReadLine()!);
+return userData;
+}
+
+int [] getRandomNumber(int length, int start,int end)
+{
+int[] result = new int[length];
+for (int i = 0; i < length; i++)
+{
+result[i] = new Random().Next(start, end + 1);
+}
+return result;
+}
+
+
+int getEvenNumbers(int[] array)
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] % 2 == 0)
+{
+count++;
+}
+
+}
+return count;
+}
+
+void printArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i]);
+if (i < array.Length - 1)
+{
+Console.Write(",");
+}
+}
+Console.WriteLine("]");
+}
+
+
+Console.Clear();
+int length = getUserData("Введите длину массива");
+int []array = getRandomNumber(length,100,999);
+printArray(array);
+int arrayCheck = getEvenNumbers(array);
+Console.WriteLine();
+Console.WriteLine(arrayCheck);
---
+++ zad_36/Program.cs
@@ -0,0 +1,51 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+int [] generatArray(int length,int start,int end)
+{
+int [] result = new int[length];
+for (int i = 0; i < length; i++)
+{
+result[i] = new Random().Next(start,end + 1);
+}
+return result;
+}
+
+
+int getSumNumbers(int[] array)
+{
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+if (i % 2 != 0)
+{
+sum = sum + array[i];
+}
+
+}
+return sum;
+}
+
+void printArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i]);
+if (i < array.Length - 1)
+{
+Console.Write(",");
+}
+}
+Console.WriteLine("]");
+}
+
+int [] array = generatArray(4,0,20);
+printArray(array);
+Console.WriteLine();
+int sum = getSumNumbers(array);
+Console.WriteLine($"cумма нечетных позиций в массиве = {sum}");
